5aef02859e9e5531d4fa5bd3d566822c0ac1c088
Oscar Moore <oscarmoore@Oscars-MacBook-Pro.local>
2026-06-05 06:44:46 -0400

    Added section to solve potential race condition

--- src/main.c
+++ src/main.c
@@ -19,6 +19,8 @@ PID PIDMotor1;
 PID PIDMotor2; 
 
 int main(void) {
+    int32_t PulseCountDTMotor1Local = 0; 
+    int32_t PulseCountDTMotor2Local = 0; 
     Motor1.MotorPPR = MotorPPR12vDC;
     Motor2.MotorPPR = MotorPPR12vDC; 
     Motor1.RPMSetPoint = 150; 
@@ -37,17 +39,20 @@ int main(void) {
     while(1){
         // PID Interval 
         if (ISRFlag == 1){
-            Motor1.MotorRPM = ((float)abs(PulseCountDTMotor1) / Motor1.MotorPPR) * (60.0f / TimeDTSec);
-            Motor2.MotorRPM = ((float)abs(PulseCountDTMotor2) / Motor2.MotorPPR) * (60.0f / TimeDTSec);
+            NVIC_DisableIRQ(TIM1_BRK_TIM9_IRQn);
+            PulseCountDTMotor1Local = PulseCountDTMotor1;
+            PulseCountDTMotor2Local = PulseCountDTMotor2;
+            ISRFlag = 0; 
+            NVIC_EnableIRQ(TIM1_BRK_TIM9_IRQn);
 
+            Motor1.MotorRPM = ((float)abs(PulseCountDTMotor1Local) / Motor1.MotorPPR) * (60.0f / TimeDTSec);
+            Motor2.MotorRPM = ((float)abs(PulseCountDTMotor2Local) / Motor2.MotorPPR) * (60.0f / TimeDTSec);
 
             PIDControl(&Motor1, &PIDMotor1); 
             PIDControl(&Motor2, &PIDMotor2); 
 
             SetPWM(&Motor1);
             SetPWM(&Motor2);
-
-            ISRFlag = 0; 
 
         }
 
